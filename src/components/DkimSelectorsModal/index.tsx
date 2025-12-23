658dff4a1ea650615d68c8e41c86e50d348bae5e
Jared Casner <jcasner@gmail.com>
2025-12-23 08:02:54 -0600

    fix issues with translations

--- src/components/DkimSelectorsModal/index.tsx
+++ src/components/DkimSelectorsModal/index.tsx
@@ -163,7 +163,7 @@ const DkimSelectorsModal: React.FC<DkimSelectorsModalProps> = ({
               aria-hidden='true'
             >
               <path
-                d='M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm1 15h-2v-2h2v2z m0-4h-2V7h2v6z'
+                d='M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm1 15h-2v-2h2v2zm0-4h-2V7h2v6z'
               />
             </svg>
             <p>
@@ -177,7 +177,7 @@ const DkimSelectorsModal: React.FC<DkimSelectorsModalProps> = ({
 
           {selectors.length > 0 && (
             <div className='dkim-selectors-section'>
-              <h4>{t('dkimModal.currentSelectors')} ({selectors.length})</h4>
+              <h4>{t('dkimModal.currentSelectors', { count: selectors.length })}</h4>
               <ul className='dkim-selectors-list'>
                 {selectors.map((selector) => (
                   <li key={selector} className='dkim-selector-item'>
@@ -186,7 +186,7 @@ const DkimSelectorsModal: React.FC<DkimSelectorsModalProps> = ({
                       className="dkim-remove-btn"
                       data-testid={`remove-selector-${selector}`}
                       onClick={() => handleRemoveSelector(selector)}
-                      aria-label={`Remove ${selector}`}
+                      aria-label={`${t('dkimModal.removeSelector', { selector })}`}
                     >
                       <svg viewBox='0 0 24 24' fill='currentColor' aria-hidden='true'>
                         <path
